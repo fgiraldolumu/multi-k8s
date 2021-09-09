1ec6e5a873549ae29df8744c1dd4ad5d4e08df67
fgiraldolumu <fgiraldo@lumu.io>
2021-09-09 12:29:36 -0500

    Changing dockerhub path to fgiraldolumu

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
-docker build -t cygnetops/multi-client-k8s:latest -t cygnetops/multi-client-k8s:$SHA -f ./client/Dockerfile ./client
-docker build -t cygnetops/multi-server-k8s-pgfix:latest -t cygnetops/multi-server-k8s-pgfix:$SHA -f ./server/Dockerfile ./server
-docker build -t cygnetops/multi-worker-k8s:latest -t cygnetops/multi-worker-k8s:$SHA -f ./worker/Dockerfile ./worker
+docker build -t fgiraldolumu/multi-client-k8s:latest -t fgiraldolumu/multi-client-k8s:$SHA -f ./client/Dockerfile ./client
+docker build -t fgiraldolumu/multi-server-k8s-pgfix:latest -t fgiraldolumu/multi-server-k8s-pgfix:$SHA -f ./server/Dockerfile ./server
+docker build -t fgiraldolumu/multi-worker-k8s:latest -t fgiraldolumu/multi-worker-k8s:$SHA -f ./worker/Dockerfile ./worker
 
 docker push fgiraldolumu/multi-client-k8s:latest
 docker push fgiraldolumu/multi-server-k8s-pgfix:latest
@@ -11,6 +11,6 @@ docker push fgiraldolumu/multi-server-k8s-pgfix:$SHA
 docker push fgiraldolumu/multi-worker-k8s:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=cygnetops/multi-server-k8s-pgfix:$SHA
-kubectl set image deployments/client-deployment client=cygnetops/multi-client-k8s:$SHA
-kubectl set image deployments/worker-deployment worker=cygnetops/multi-worker-k8s:$SHA
+kubectl set image deployments/server-deployment server=fgiraldolumu/multi-server-k8s-pgfix:$SHA
+kubectl set image deployments/client-deployment client=fgiraldolumu/multi-client-k8s:$SHA
+kubectl set image deployments/worker-deployment worker=fgiraldolumu/multi-worker-k8s:$SHA
